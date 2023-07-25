268e9b1fe0d80e9816407656340ea22f3919f166
AllPav95 <136779078+AllPav95@users.noreply.github.com>
2023-07-25 21:26:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import sapper_game
-import sys
 import menu
 
-def main():
-    menu.main()
-
 if __name__ == '__main__':
-
-     if len(sys.argv) == 3:
-        size = int(sys.argv[1])
-        mines = int(sys.argv[2])
-        sapper_game.startGame(size, mines)
-     else:
-         print("Usage: python sapper_game.py <size> <mines>")
+    menu.mainMenu()
